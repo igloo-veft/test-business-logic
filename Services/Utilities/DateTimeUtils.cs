39aa4def3b98e907c78698608f442e15aa1efd50
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-09-18 10:54:13 +0000

    fixed logic for leap year check

--- Services/Utilities/DateTimeUtils.cs
+++ Services/Utilities/DateTimeUtils.cs
@@ -5,9 +5,14 @@ namespace CoursesAPI.Services.Utilities
 		public static bool IsLeapYear(int year)
 		{
 			if(year % 4 == 0) {
+				if(year % 100 == 0) {
+					if(year % 400 == 0) {
+						return true;
+					}
+					return false;
+				}
 				return true;
 			}
-			// TODO: add your logic here!!!1!!!one!!!eleven
 			return false;
 		}
 	}
